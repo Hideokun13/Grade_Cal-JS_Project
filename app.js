0a88e6b7e6afa5c8d8ba52a5a22acf8561877c78
Hideokun13 <boypeerawit@hotmail.com>
2021-02-03 14:04:48 +0700

    Fix "condition score >=" bug eg.(score = 75 the program give grade "F" instend "B")

--- app.js
+++ app.js
@@ -18,26 +18,32 @@ function CalGrade(data){
         let temp = data[i].split(",")
         let score = temp[2]
         let grade = ""
-        if(score >= 85){
+        if(temp[1] != "-" && score >= 85){
             grade = "A"
         }
-        else if(score >= 80 && score < 85){
+        else if(temp[1] != "-" && score >= 80 && score < 85){
             grade = "B+"
         }
-        else if(score >= 75 && score < 80){
+        else if(temp[1] != "-" && score >= 75 && score < 80){
             grade = "B"
         }
-        else if(score >= 70 && score < 75){
+        else if(temp[1] != "-" && score >= 70 && score < 75){
             grade = "C+"
         }
-        else if(score >= 65 && score < 70){
+        else if(temp[1] != "-" && score >= 65 && score < 70){
             grade = "C"
         }
-        else if(score >= 60 && score < 65){
+        else if(temp[1] != "-" && score >= 60 && score < 65){
             grade = "D+"
         }
-        else if (score >= 55 && score < 60){
+        else if (temp[1] != "-" && score >= 55 && score < 60){
             grade = "D"
+        }
+        else if (temp[1] == "-" && score >= 50){
+            grade = "S"
+        }
+        else if (temp[1] == "-" && score < 50){
+            grade = "U"
         }
         else {
             grade = "F"
