eb3ac0089081052e89261dd3b1c6a87fff222445
Hideokun13 <boypeerawit@hotmail.com>
2021-02-04 23:50:14 +0700

    Use try-catch to prevent null data

--- app.js
+++ app.js
@@ -75,56 +75,65 @@ function CalGrade(){
     }
 }
 function CalGPA(){
-    let sumScore = 0
-    let totalCredit = 0
-    for(let i = 0; i < gradeData.length; i++){
-        let temp = gradeData[i].split(",")
-        let credit = parseInt(temp[1])
-        let grade = temp[3]
-            switch(grade){
-                case "A": {
-                    sumScore += (4 * credit)
-                    totalCredit += credit
-                } break;
-                case "B+":{
-                    sumScore += (3.5 * credit)
-                    totalCredit += credit
-                } break;
-                case "B":{
-                    sumScore += (3 * credit)
-                    totalCredit += credit
-                } break;
-                case "C+":{
-                    sumScore += (2.5 * credit)
-                    totalCredit += credit
-                } break;
-                case "C":{
-                    sumScore += (2 * credit)
-                    totalCredit += credit
-                } break;
-                case "D+":{
-                    sumScore += (1.5 * credit)
-                    totalCredit += credit
-                } break;
-                case "D":{
-                    sumScore += (1 * credit)
-                    totalCredit += credit
-                } break;
-                case "F":{
-                    sumScore += 0
-                    totalCredit += credit
-                } break;
-                case "S":{
-                    sumScore += 0
-                } break;
-                case "U":{
-                    sumScore += 0
-                } break;
+    try{
+        let sumScore = 0
+        let totalCredit = 0
+        for(let i = 0; i < gradeData.length; i++){
+            let temp = gradeData[i].split(",")
+            let credit = parseInt(temp[1])
+            let grade = temp[3]
+            if(temp[3] != "-"){
+                switch(grade){
+                    case "A": {
+                        sumScore += (4 * credit)
+                        totalCredit += credit
+                    } break;
+                    case "B+":{
+                        sumScore += (3.5 * credit)
+                        totalCredit += credit
+                    } break;
+                    case "B":{
+                        sumScore += (3 * credit)
+                        totalCredit += credit
+                    } break;
+                    case "C+":{
+                        sumScore += (2.5 * credit)
+                        totalCredit += credit
+                    } break;
+                    case "C":{
+                        sumScore += (2 * credit)
+                        totalCredit += credit
+                    } break;
+                    case "D+":{
+                        sumScore += (1.5 * credit)
+                        totalCredit += credit
+                    } break;
+                    case "D":{
+                        sumScore += (1 * credit)
+                        totalCredit += credit
+                    } break;
+                    case "F":{
+                        sumScore += 0
+                        totalCredit += credit
+                    } break;
+                    case "S":{
+                        sumScore += 0
+                    } break;
+                    case "U":{
+                        sumScore += 0
+                    } break;
+                }
+            }
+            else{
+                CalGrade()
             }
         }
-    document.getElementById("Total_Credit").innerHTML = "หน่วยกิตรวม: " + totalCredit
-    document.getElementById("GPA_Detail").innerHTML = "เกรดเฉลี่ย GPA: " + FloorGPA((sumScore / totalCredit))
-    
+        document.getElementById("Total_Credit").innerHTML = "หน่วยกิตรวม: " + totalCredit
+        document.getElementById("GPA_Detail").innerHTML = "เกรดเฉลี่ย GPA: " + FloorGPA((sumScore / totalCredit))
+    }
+    catch(err){
+        alert("กรุณาใส่ข้อมูลก่อนคำนวณ")
+    }
 }
 function ClearTable(){
     let table = document.getElementById("GradeTable")
